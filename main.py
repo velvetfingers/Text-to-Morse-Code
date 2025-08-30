948d1b42c80b1d9ae2c3303e6c3bf7ce51df13a1
Kara Silvester <monthly.cactus0w@icloud.com>
2025-08-30 19:28:46 +0100

    Add morse-to-text functionality

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
 #   Text to Morse Code Converter
-#   by VelvetFingers ©️
+#   by VelvetFingers - a python student ©️
 
 from data import MORSE_CODES, LOGO
 
 is_on = True
-print(f"{LOGO}\n-- --- .-. ... ./-.-. --- -.. ./-.-. --- -. ...- . .-. - . .-.\nThis program converts a message into morse code!")
+print(f"{LOGO}\n-- --- .-. ... ./-.-. --- -.. ./-.-. --- -. ...- . .-. - . .-.\nThis program converts a message to and from morse code!")
 
 
 while is_on:
+    output = ""
+    morse_char_to_letter = dict((v, k) for k,v in MORSE_CODES.items())
+
+    direction = int(input("\nChoose a direction:\n[1] From text to morse code\n[2] From morse code to text\nEnter a choice (e.g. 1) => "))
     print("\nPlease note: this program only accepts alphanumerical characters, do not use any symbols.")
-    message = input("Enter your message:\n")
+    message = input("Enter your message:\n" if direction == 1 else "Paste the morse code:\n")
 
     try:
-        # [1] Split the message into a space-separated list
-        # [2] Add each letter (morse code converted) into a new list
-        # [3] Convert to a string, each morse-code letter separated by a space
-        # [4] Convert to a string, each morse-code word separated by a forward-slash
-        output = "/".join(
-            [" ".join(MORSE_CODES[letter.upper()] for letter in word)
-            for word in message.split()]
-        )
+        # plaintext --> morse-code
+        if direction == 1:
+            output = "/".join(
+                [" ".join(MORSE_CODES[letter.upper()] for letter in word)
+                for word in message.split()]
+            )
+
+        # morse-code --> plaintext
+        elif direction == 2:
+            output = (
+                " ".join(
+                    ["".join(
+                        [morse_char_to_letter[letter] for letter in word.split()]
+                    ) for word in message.split("/")]
+                )
+            ).capitalize()
     except KeyError:
-        message = input("\nYour message included an illegal character. Please enter a new message using only letters or numbers:\n")
-        output = "/".join(
-            [" ".join(MORSE_CODES[letter.upper()] for letter in word)
-             for word in message.split()]
-        )
+        message = input("\nYour message included an illegal character. Please try again using only letters or numbers.\n")
+        break
     else:
         # Print output to console
         print(f"\nYour converted message:\n{output}")
